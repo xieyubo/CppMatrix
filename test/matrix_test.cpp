0ad9d95dcdfbe81bd0a8d3d2b6e6ee9414a9c6e7
xyb <xyb@xyb.name>
2024-11-17 10:50:18 -0800

    Add test

--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -10,12 +10,18 @@ TEST(MatrixTest, DefaultConstructor)
     Matrix x {};
     ASSERT_EQ(x.Row(), 0);
     ASSERT_EQ(x.Column(), 0);
+    ASSERT_FALSE(!!x);
 }
 
 TEST(MatrixTest, SetAbsoluteFloat)
 {
     Matrix x {};
     x = 1.123f;
+    ASSERT_TRUE(!!x);
     ASSERT_EQ(x.Row(), 1);
     ASSERT_EQ(x.Column(), 1);
+
+    auto data = x.Read();
+    ASSERT_EQ(data.size(), 1);
+    ASSERT_FLOAT_EQ(data[0], 1.123f);
 }
